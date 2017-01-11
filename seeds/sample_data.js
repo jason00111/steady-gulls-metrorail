e1f20bcbe210a8a4ca43cd6fac9668b5f7d0cf19
jason00111 <emberleyjason@gmail.com>
2017-01-11 13:31:58 -0800

    Complete seed file, add db:seed script

--- seeds/sample_data.js
+++ seeds/sample_data.js
@@ -5,26 +5,29 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('stations').insert({id: 0, colName:'Downtown'}),
-        knex('stations').insert({id: 1, colName:'Elm Street'}),
-        knex('stations').insert({id: 2, colName:'Forest Gardens'}),
-        knex('stations').insert({id: 3, colName:'Annex'}),
-        knex('stations').insert({id: 4, colName:'10th Ave'}),
-        knex('stations').insert({id: 5, colName:'Waterfront'}),
-        knex('stations').insert({id: 6, colName:'Colosseum'}),
-        knex('stations').insert({id: 7, colName:'Central Station'}),
-        knex('stations').insert({id: 8, colName:'Parkside'}),
-        knex('stations').insert({id: 9, colName:'Grand Boulevard'}),
-        knex('stations').insert({id: 10, colName:'Monument Valley'}),
-        knex('stations').insert({id: 11, colName:'Museum Isle'})
+        knex('stations').insert({id: 0, name: 'Downtown'}),
+        knex('stations').insert({id: 1, name: 'Elm Street'}),
+        knex('stations').insert({id: 2, name: 'Forest Gardens'}),
+        knex('stations').insert({id: 3, name: 'Annex'}),
+        knex('stations').insert({id: 4, name: '10th Ave'}),
+        knex('stations').insert({id: 5, name: 'Waterfront'}),
+        knex('stations').insert({id: 6, name: 'Colosseum'}),
+        knex('stations').insert({id: 7, name: 'Central Station'}),
+        knex('stations').insert({id: 8, name: 'Parkside'}),
+        knex('stations').insert({id: 9, name: 'Grand Boulevard'}),
+        knex('stations').insert({id: 10, name: 'Monument Valley'}),
+        knex('stations').insert({id: 11, name: 'Museum Isle'}),
 
-        knex('passengers').insert({id: 0, colName:'Digna'})
-        knex('passengers').insert({id: 1, colName:'Kenton'})
-        knex('passengers').insert({id: 2, colName:'Min'})
-        knex('passengers').insert({id: 3, colName:'Toni'})
-        knex('passengers').insert({id: 4, colName:'Danial'})
+        knex('passengers').insert({id: 0, name: 'Digna'}),
+        knex('passengers').insert({id: 1, name: 'Kenton'}),
+        knex('passengers').insert({id: 2, name: 'Min'}),
+        knex('passengers').insert({id: 3, name: 'Toni'}),
+        knex('passengers').insert({id: 4, name: 'Danial'}),
 
-        knex('trains').insert({id: 4, colName:'Danial'})
+        knex('trains').insert({id: 0, capacity: 200}),
+        knex('trains').insert({id: 1, capacity: 200}),
+        knex('trains').insert({id: 2, capacity: 200}),
+        knex('trains').insert({id: 3, capacity: 200})
       ]);
     });
 };
