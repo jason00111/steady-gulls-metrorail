724aea0eeea629a76bca08482270e6d19716ae85
John Hopkins <johnthopkins@gmail.com>
2017-01-11 11:09:12 -0800

    trains db controller created && repl

--- db/train.js
+++ db/train.js
@@ -1,21 +1,33 @@
-const knex = require('knex')
+const knexconfig = require('../knexfile');
+const knex = require('knex')(knexconfig[process.env.NODE_ENV])
+console.log(knexconfig);
+
 
 function updateTrain (options) {
   knex('trains')
     .where('number', options.number)
-
-  knex('trains')
-    .where('number', options.number)
-    .update(options)
+    .then(result => {
+      if(result.length === 0){
+        return knex('trains')
+          .insert(options)
+      } else{
+        return knex('trains').where('number', options.number).update(options)
+      }
+    }).catch(error => console.log('ERROR!!!!>>>>!!!!>>>!>!>!>!>', error))
 }
 
+function deleteTrain (options){
+  knex('trains')
+    .where('number', options.number).del()
+      .catch(error => console.log(error))
+}
 
+function getTrainByNumber (options){
+  return knex('trains').where('number', options.number)
+}
 
-
-
-
-module.exports = {updateTrain}
+module.exports = { updateTrain, deleteTrain, getTrainByNumber }
 
 //
-// deleteTrain
+//
 // getTrainByNumber //change to getTrain
