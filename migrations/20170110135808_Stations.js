eca15b6e943fcad5b48a87518acb4fb713568524
jason00111 <emberleyjason@gmail.com>
2017-01-11 13:31:58 -0800

    Complete seed file, add db:seed script

--- migrations/20170110135808_Stations.js
+++ migrations/20170110135808_Stations.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('stations', table => {
     table.integer('id')
+    table.string('name')
     table.json('passengers')
   })
 };
