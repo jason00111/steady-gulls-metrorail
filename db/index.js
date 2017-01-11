d31430434b3a2b0ac9cb6371604de05d5ac57248
jason00111 <emberleyjason@gmail.com>
2017-01-11 13:53:04 -0800

    Combine db functions

--- db/index.js
+++ db/index.js
@@ -1,5 +1,19 @@
+const {updateRecord, deleteRecord, getRecord} = require('./record')
+
 module.exports = {
-  passenger: require('./passenger'),
-  train: require('./train'),
-  station: require('./station')
+  passenger: {
+    updatePassenger: updateRecord.bind(this, 'passengers'),
+    deletePassenger: deleteRecord.bind(this, 'passengers'),
+    getPassenger: getRecord.bind(this, 'passengers')
+  },
+  train: {
+    updateTrain: updateRecord.bind(this, 'trains'),
+    deleteTrain: deleteRecord.bind(this, 'trains'),
+    getTrain: getRecord.bind(this, 'trains')
+  },
+  station: {
+    updateStation: updateRecord.bind(this, 'stations'),
+    deleteStation: deleteRecord.bind(this, 'stations'),
+    getStation: getRecord.bind(this, 'stations')
+  }
 }
